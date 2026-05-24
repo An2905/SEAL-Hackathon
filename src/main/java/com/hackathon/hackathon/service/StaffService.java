0b261675eee0d8b00522ea176231839b9e3e1a53
NguyenQuocAn <quocannguyen385@gmail.com>
2026-05-24 20:55:43 +0700

    Jmockit Added

--- src/main/java/com/hackathon/hackathon/service/StaffService.java
+++ src/main/java/com/hackathon/hackathon/service/StaffService.java
@@ -1,19 +1,12 @@
 package com.hackathon.hackathon.service;
 
-import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class StaffService {
-private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    @Autowired
-    private DataSource dataSource;
-    @Autowired
-    private EmailService emailService;
+
 
 //region CREATE ACCOUNTS
 
