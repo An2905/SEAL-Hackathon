32cee5215f0e27d071e695a6bf8827c05001db35
GHP1991 <danggiahuy671@gmail.com>
2026-05-25 11:36:33 +0700

    Change event status & config.env added

--- src/main/java/com/hackathon/hackathon/controller/StaffController.java
+++ src/main/java/com/hackathon/hackathon/controller/StaffController.java
@@ -1,11 +1,27 @@
 package com.hackathon.hackathon.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hackathon.hackathon.dto.ChangeEventStatusRequest;
+import com.hackathon.hackathon.dto.UpdatePasswordRequest;
+import com.hackathon.hackathon.service.AuthService;
+import com.hackathon.hackathon.service.StaffService;
 @RestController
 @RequestMapping("/api/staff")
 @CrossOrigin("*")
 public class StaffController {
+
+    @Autowired
+    private StaffService staffService;
+
+    @PutMapping("/updatestatus")
+    public String changeEventStatus(@RequestHeader("Authorization")String authHeader,@RequestBody ChangeEventStatusRequest request) {
+        return staffService.changeEventStatus(authHeader,request);
+    }
 
 }
---
+++ src/main/java/com/hackathon/hackathon/dto/ChangeEventStatusRequest.java
@@ -0,0 +1,9 @@
+package com.hackathon.hackathon.dto;
+import lombok.Data;
+
+@Data
+public class ChangeEventStatusRequest {
+    private String eventId;
+    
+    private String newStatus;
+}
