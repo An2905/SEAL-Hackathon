2751d655edb8b6265c8a0b8d3152d1f60d1dff1c
Dat Truong <JasonTruong3005@gmail.com>
2026-05-31 11:48:57 +0700

    getmapping mentorviewassignedevent

--- backend/src/main/java/com/hackathon/hackathon/controller/MentorController.java
+++ backend/src/main/java/com/hackathon/hackathon/controller/MentorController.java
@@ -1,18 +1,19 @@
 package com.hackathon.hackathon.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hackathon.hackathon.model.dto.response.EventSummaryResponse;
 import com.hackathon.hackathon.service.MentorService;
-
-
-
 
 @RestController
 @RequestMapping("/api/mentor")
 @CrossOrigin("*")
-
 public class MentorController {
     private final MentorService mentorService;
 
@@ -20,5 +21,9 @@ public class MentorController {
         this.mentorService = mentorService;
     }
 
-
+    @GetMapping("/events")
+    public List<EventSummaryResponse> getAssignedEvents(
+            @RequestHeader(value = "Authorization", required = false) String authHeader) {
+        return mentorService.getAssignedEvents(authHeader);
+    }
 }
