fad4067ae9fc0da2dc12b4179cb6f41e121c030e
NguyenQuocAn <quocannguyen385@gmail.com>
2026-06-17 14:11:17 +0700

    Spotless Fixed

--- backend/src/main/java/com/hackathon/hackathon/controller/AuthController.java
+++ backend/src/main/java/com/hackathon/hackathon/controller/AuthController.java
@@ -15,13 +15,12 @@ import com.hackathon.hackathon.service.AuthService;
 import com.hackathon.hackathon.service.GithubOauthService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping(value = "/api/auth", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
