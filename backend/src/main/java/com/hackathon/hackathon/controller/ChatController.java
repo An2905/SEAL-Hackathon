f83ac52f59ee046b340478091119dcb386863b90
Dat Truong <JasonTruong3005@gmail.com>
2026-06-22 00:41:40 +0700

    update

--- backend/src/main/java/com/hackathon/hackathon/controller/ChatController.java
+++ backend/src/main/java/com/hackathon/hackathon/controller/ChatController.java
@@ -1,11 +1,7 @@
 package com.hackathon.hackathon.controller;
 
-import com.hackathon.hackathon.model.dto.request.CreateChatRoomRequest;
-import com.hackathon.hackathon.model.dto.request.OpenChatRoomRequest;
-import com.hackathon.hackathon.model.dto.response.ChatMessageResponse;
-import com.hackathon.hackathon.model.dto.response.ChatRoomResponse;
-import com.hackathon.hackathon.service.ChatService;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +13,14 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hackathon.hackathon.model.dto.request.CreateChatRoomRequest;
+import com.hackathon.hackathon.model.dto.request.OpenChatRoomRequest;
+import com.hackathon.hackathon.model.dto.response.ChatMessageResponse;
+import com.hackathon.hackathon.model.dto.response.ChatRoomResponse;
+import com.hackathon.hackathon.service.ChatService;
+
+
 
 @RestController
 @RequestMapping(value = "/api/chat", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
